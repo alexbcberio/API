1053d7ac41873409c888486a799694aca0d52603
alexbcberio <alexbcberio@gmail.com>
2020-10-26 19:06:53 +0100

    Fix strings format Co-authored-by: Bas950 <me@bas950.com>

--- src/endpoints/v3/fields/langFiles.ts
+++ src/endpoints/v3/fields/langFiles.ts
@@ -3,6 +3,8 @@ import { GraphQLList, GraphQLString } from "graphql";
 import { cache } from "../../../index";
 import { langFileType } from "../types/langFiles/langFileType";
 
+let doneOnce = false;
+
 export const langFiles = {
 	type: GraphQLList(langFileType),
 	args: {
@@ -13,6 +15,8 @@ export const langFiles = {
 		return cache.get("langFiles").filter(lF => {
 			let checksToPass = 2,
 				checksThatPassed = 0;
+
+			lF.translations = formatLangFile(lF);
 
 			if (args.lang) {
 				if (lF.lang === args.lang) checksThatPassed++;
@@ -26,3 +30,15 @@ export const langFiles = {
 		});
 	}
 };
+
+function formatLangFile(langFile) {
+	return Object.assign(
+		{},
+		...Object.keys(langFile.translations).map(translationKey => {
+			const newKey = translationKey.replace(/[_]/g, ".");
+			return {
+				[newKey]: langFile.translations[translationKey]
+			};
+		})
+	);
+}
